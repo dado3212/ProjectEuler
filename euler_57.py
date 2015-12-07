0eaecc251d7f823eaff73952fd3a826311fde02d
Alex Beals <alexcbeals@gmail.com>
2015-12-06 23:50:08 -0800

    Problem 57

---
+++ euler_57.py
@@ -0,0 +1,40 @@
+# Problem 57
+# Answer: 153
+
+'''
+It is possible to show that the square root of two can be expressed as an infinite continued fraction.
+
+sqrt 2 = 1 + 1/(2 + 1/(2 + 1/(2 + ... ))) = 1.414213...
+
+By expanding this for the first four iterations, we get:
+
+1 + 1/2 = 3/2 = 1.5
+1 + 1/(2 + 1/2) = 7/5 = 1.4
+1 + 1/(2 + 1/(2 + 1/2)) = 17/12 = 1.41666...
+1 + 1/(2 + 1/(2 + 1/(2 + 1/2))) = 41/29 = 1.41379...
+
+The next three expansions are 99/70, 239/169, and 577/408, but the eighth expansion, 1393/985, is the first example where the number of digits in the numerator exceeds the number of digits in the denominator.
+
+In the first one-thousand expansions, how many fractions contain a numerator with more digits than denominator?
+'''
+
+from fractions import Fraction
+
+expansions = {}
+
+def expansion(n):
+	if n == -1:
+		return 0
+	if n in expansions:
+		return expansions[n]
+	else:
+		x = Fraction(1,2 + expansion(n-1))
+		expansions[n] = x
+		return 1 + x
+
+count = 0
+for i in xrange(0,1001):
+	a = expansion(i)
+	if len(str(a.numerator)) > len(str(a.denominator)):
+		count += 1
+print count
